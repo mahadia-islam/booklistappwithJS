8929dbf2a47c0f604b0211e377a5e85ecc76feea
mahadia-islam <74854813+mahadia-islam@users.noreply.github.com>
2021-05-23 14:56:38 +0600

    all file are changed

---
+++ assets/custom.js
@@ -0,0 +1,10 @@
+let bookName = $('#bookname').val();
+let author = $('#author').val();
+let year = $('#year').val();
+let btn = $('.btn');
+let bookList = $('#book-list');
+
+btn.click(function (form) {
+    form.preventDefault();
+    console.log('hello world');
+});
